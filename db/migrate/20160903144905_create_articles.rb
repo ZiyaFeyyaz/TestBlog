878144b5fdbbbe6ac1e865c0a624b92fd98fdf60
Ziya Feyyaz <ziya.feyyaz@yandex.ru>
2016-09-03 23:42:46 +0300

    Modify create_article migration

--- db/migrate/20160903144905_create_articles.rb
+++ db/migrate/20160903144905_create_articles.rb
@@ -1,10 +1,14 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :articles do |t|
       t.string :title
       t.text :text
 
       t.timestamps
     end
+  end
+ 
+  def down
+    drop_table :articles
   end
 end
