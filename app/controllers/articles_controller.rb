e5f41e24bb35073035d74738e2ab4c2d6e581ef0
Ziya Feyyaz <ziya.feyyaz@yandex.ru>
2016-09-03 21:15:04 +0300

    Add authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "feyyaz", password: "zzz", except: [:index, :show]
+
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
