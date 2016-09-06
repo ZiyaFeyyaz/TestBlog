f50c80cbcf5419599bc2c8a8f4cb3d5915daacae
Ziya Feyyaz <ziya.feyyaz@yandex.ru>
2016-09-06 16:10:45 +0300

    Fix swagger congig

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,11 @@
-class Api::V0::WelcomeController < ApplicationController
+class WelcomeController < ApplicationController
   def index
   end
 
   swagger_controller :welcome, "Welcome"
 
-  swagger_api :create do
-    summary "Creates a new User"
-    param :form, :first_name, :string, "First name"
-    param :form, :last_name, :string, "Last name"
-    param :form, :email, :string, :required, "Email address"
-    param :form, :password, :string, :required, "Password"
-    param :form, :password_confirmation, :string, :required, "Password Confirmation"
+  swagger_api :index do
+    summary "Fetches all users"
     response :unauthorized
     response :not_acceptable
   end
