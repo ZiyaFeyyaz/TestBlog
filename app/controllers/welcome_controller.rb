81de91678138e38ce4bec6ea2d5b6f2cbaf5b31f
Ziya Feyyaz <ziya.feyyaz@yandex.ru>
2016-09-06 12:47:49 +0300

    Add gem swagger-docs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,17 @@
-class WelcomeController < ApplicationController
+class Api::V0::WelcomeController < ApplicationController
   def index
+  end
+
+  swagger_controller :welcome, "Welcome"
+
+  swagger_api :create do
+    summary "Creates a new User"
+    param :form, :first_name, :string, "First name"
+    param :form, :last_name, :string, "Last name"
+    param :form, :email, :string, :required, "Email address"
+    param :form, :password, :string, :required, "Password"
+    param :form, :password_confirmation, :string, :required, "Password Confirmation"
+    response :unauthorized
+    response :not_acceptable
   end
 end
