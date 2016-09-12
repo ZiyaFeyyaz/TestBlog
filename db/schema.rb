41c1a9d5a4342f86709371cb5753f6d9dd71ef96
Ziya Feyyaz <ziya.feyyaz@yandex.ru>
2016-09-12 17:55:17 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912112838) do
+ActiveRecord::Schema.define(version: 20160912145111) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -39,8 +39,13 @@ ActiveRecord::Schema.define(version: 20160912112838) do
 
   create_table "events", force: :cascade do |t|
     t.string   "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "summary"
+    t.string   "location"
+    t.string   "description"
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "users", force: :cascade do |t|
